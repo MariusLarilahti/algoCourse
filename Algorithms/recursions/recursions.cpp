666e8774c94fe5312968c3a76edd37d2a9bee3f5
MariusLarilahti <fmala029@edu.xamk.fi>
2026-09-23 17:02:39 +0300

    programming tasks  3 submission

--- Algorithms/recursions/recursions.cpp
+++ Algorithms/recursions/recursions.cpp
@@ -13,7 +13,46 @@ int fibRecursive(int n)
         return fibRecursive(n - 1) + fibRecursive(n - 2);
 }
 
+int fibIterative(int n)
+{
+    int n_minus_2;
+    int n_minus_1 = 0;
+    int n_current = 1;
+
+    for (int i = 1; i < n + 1; i++) 
+    {
+        n_minus_2 = n_minus_1;
+        n_minus_1 = n_current;
+        n_current = n_minus_2 + n_minus_1;
+    }
+    return n_current;
+}
+
+//A(0,n) = n + 1
+//A(m, 0) = A(m - 1, 1)
+//A(m, n) = A(m - 1, A(m, n - 1))
+int ackermann(int m, int n) 
+{
+    if (m < 0 || n < 0)
+        return 0;
+
+    if (m == 0) 
+        return n + 1;
+
+    if (n == 0)
+        return ackermann(m - 1, 1);
+
+    else
+        return ackermann(m - 1, ackermann(m, n - 1));
+}
+
 int main()
 {
-    cout << fibRecursive(6);
+    cout << "Ackermann(2,2): " << ackermann(2, 2) << endl;
+    int x;
+    cout << "Please give an integer for fibonacci calculation: ";
+    cin >> x;
+    cout << "Recursive: " << fibRecursive(x) << endl;
+    cout << "Iterative: " << fibIterative(x) << endl;
+    
 }
